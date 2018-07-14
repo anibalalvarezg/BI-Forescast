40e0ae1b5213f952e7a121fab65ec6b4c723895a
Aníbal Álvarez González <anibal.alvarezg@gmail.com>
2018-07-14 00:59:12 -0400

    Componentes listo

--- main.m
+++ main.m
@@ -1,14 +1,25 @@
 # MAIN #
 load_data
 
-x = x_tr';
+#x = x_tr';
+x = [8 6 3 9 0 4 3];
 
 #Numero filas
-L = 20; 
+L = 3; 
 N = columns(x);
+
+#Numero Columnas
 M=N-L+1;
 
 H = hankel(x,L);
 
 [U,S,V] = svd(H);
 S = diag(S);
+
+C = zeros(L,N);
+for i=1:L
+  A =(S(i) * U(1:end,i))* V(1:end,i)';
+  C(i,1:end) = [A(1,1:M) A(2:L,M)'];
+endfor
+
+C(1,1:end)+ C(2,1:end) + C(3,1:end)
