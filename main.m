df6dee5a7c25842e3dd719d2e0b502abaddd2ec1
Aníbal Álvarez González <anibal.alvarezg@gmail.com>
2018-07-13 23:56:40 -0400

    Hankel listo

--- main.m
+++ main.m
@@ -1,14 +1,7 @@
 # MAIN #
-#load_data
+load_data
 
 
-x = [1 2 3 4 5 6 7];
-L = 3; 
-M = 5; 
-
-H = zeros(L,M);
-for i=1:L
-  for j=1:M
-    H(i,j) = x(i+j-1)
-  end
-end
+x = x_tr';
+L = 20; 
+H = hankel(x,L);
