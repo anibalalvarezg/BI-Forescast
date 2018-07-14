efe23d6a200b7ae87a162591462db667da744694
Aníbal Álvarez González <anibal.alvarezg@gmail.com>
2018-07-13 22:57:42 -0400

    Normalización y división de data.

--- load_data.m
+++ load_data.m
@@ -1,1 +1,16 @@
 # Load data #
+data = load('TSerieEjemplo.txt');
+dim_data = size(data,1);
+
+min_value = min(data);
+max_value = max(data);
+
+for i=1:dim_data
+  data(i) = (data(i)-min_value) / (max_value - min_value);
+end
+
+
+dim_data_tr = floor(dim_data * 0.8);
+x_tr = data(1:dim_data_tr);
+y_tr = data(dim_data_tr+1:end); 
+
