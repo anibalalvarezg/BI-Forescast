8d9fb1593df1b50468b275e47d8bd41a9668038d
Aníbal Álvarez González <anibal.alvarezg@gmail.com>
2018-07-18 01:17:06 -0400

    update

--- AAR/main.m
+++ AAR/main.m
@@ -7,10 +7,12 @@ h = 1;
 main_train(L,m,h,x_tr);
 
 [y_pred] = main_test(L,m,h,x_tst);
+
 y_tr_tst = x_tst(m+1:end);
+y_pred = y_pred';
 
 hold on;
   plot(1:columns(y_tr_tst),y_tr_tst);
-  plot(1:columns(y_pred'),y_pred',"r");
+  plot(1:columns(y_pred),y_pred,"r");
   legend('Actual Value','Estimated Value');
 hold off;
