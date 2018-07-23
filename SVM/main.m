ce89430a0b4d8b48bf69a43c073aae7d1ad6fd3b
Unknown <fabian.vergara.l@mail.pucv.cl>
2018-07-23 02:46:52 -0400

    SVM con memoria

--- SVM/main.m
+++ SVM/main.m
@@ -11,15 +11,15 @@ testing = true;
 
 %mejMem = memRest(lags(1), memories);
 
-mejorMem = 10;
+mejorMem = 20;
 %svm.Gamma = 4;
 %svm.Sigma2 = 2;
 
 function [Param] = svm_grid();
-for i = 1:5
+for i = 1:6
   c(i) = 2.^i
 end
-for i2 = 1:5
+for i2 = 1:4
   j = -2:1:2;
   g(i2) = 10.^j(i2)
 end
@@ -49,27 +49,29 @@ for i = 1:MaxIter
     for lag=1:length(lags)
       [x_L x_H] = hsvd(x_tr', lags(lag));
       results{lag}.lag = lags(lag);
-      %results{lag}.error = [];
-      %results{lag}.zv = [];
-
-      for h=1:horizonte(1):horizonte(2)
-      %fprintf("H=%d\n", h);
-        % BAJA FRECUENCIA
-        [HL, M, L] = vect_reg(mejorMem+h-1,x_L');
-        xe = HL(:, 1:(size(HL)(2)-h));
-        ye = HL(:, size(HL)(2));
-        results{lag}.L{h} = svmTrain(xe, ye, svm);
-        
-        % ALTA FRECUENCIA
-        [HH, M, L] = vect_reg(mejorMem+h-1,x_H');
-        xe = HH(:, 1:(size(HH)(2)-h));
-        ye = HH(:, size(HH)(2));
-        % Se entrena
-        results{lag}.H{h} = svmTrain(xe, ye, svm);
+      for m=1:length(memories)
+        for h=1:horizonte(1):horizonte(2)
+          % BAJA FRECUENCIA
+          [HL, M, L] = vect_reg(memories(m)+h-1,x_L');
+          xe = HL(:, 1:(size(HL)(2)-h));
+          ye = HL(:, size(HL)(2));
+          results{lag}.memory{m}.h{h}.L = svmTrain(xe, ye, svm);
+          
+          % ALTA FRECUENCIA
+          [HH, M, L] = vect_reg(memories(m)+h-1,x_H');
+          xe = HH(:, 1:(size(HH)(2)-h));
+          ye = HH(:, size(HH)(2));
+          % Se entrena
+          results{lag}.memory{m}.h{h}.H = svmTrain(xe, ye, svm);
+        end
       end
+      
     end
   end
 
+end
+if(training)
+  save(['','results.mat'],'results');
 end
 if(testing)
   load(['','results.mat']);
@@ -79,54 +81,49 @@ if(testing)
     % divide training en Alta y Baja frecuencia
     [x_L x_H] = hsvd(y', lags(lag));
     
-    %fprintf("LAG %d\n", lags(lag));
-    results{lag}.error = [];
-    % Se predice desde h=1 hasta h=dtst
-    results{lag}.zv = [];
-    for h=horizonte(1):horizonte(2)
-      %fprintf("H=%d\n", h);
-      % BAJA FRECUENCIA
-      [HL, M, L] = vect_reg(mejorMem-1,x_L');
-      %[HL, M, L] = MatrizHankel(bestMemory.memory-1,XL);
-      xv = HL(:, 1:(size(HL)(2)));
-      % Se testea
-      zvFinal = testSvm(xv,results{lag}.L{h});
-      
-      % ALTA FRECUENCIA
-      [HH, M, L] = vect_reg(mejorMem-1,x_H');
-      %[HH, M, L] = MatrizHankel(bestMemory.memory-1,XH);
-      xv = HH(:, 1:(size(HL)(2)));
-      % Se testea
-      zvFinal = zvFinal + testSvm(xv,results{lag}.H{h});
-      
-      results{lag}.zv = [results{lag}.zv; zvFinal(1,1)];
-      
-      error = (zvFinal(1,1)-y(h))**2;
-      mnse = mnse_function(zvFinal,y);
-      mae = mae_function(zvFinal,y);
-      r = r_function(zvFinal,y);
-      results{lag}.error = [results{lag}.error; error];
-      if (error < mse)
-        mse = error;
-        zvBest = zvFinal;
-        hBest = h;
-        mnseBest = mnse;
-        maeBest = mae;
-        rBest = r;
+    for m=1:length(memories)
+      for h=horizonte(1):horizonte(2)
+        results{lag}.memory{m}.h{h}.error = [];
+        results{lag}.memory{m}.h{h}.zv = [];
+        
+        % BAJA FRECUENCIA
+        [HL, M, L] = vect_reg(memories(m)-1,x_L');
+        xv = HL(:, 1:(size(HL)(2)));
+        % Se testea
+        zvFinal = testSvm(xv,results{lag}.memory{m}.h{h}.L);
+        
+        % ALTA FRECUENCIA
+        [HH, M, L] = vect_reg(memories(m)-1,x_H');
+        xv = HH(:, 1:(size(HL)(2)));
+        % Se testea
+        zvFinal = zvFinal + testSvm(xv,results{lag}.memory{m}.h{h}.H);
+        
+        results{lag}.memory{m}.h{h}.zv = [results{lag}.memory{m}.h{h}.zv; zvFinal(1,1)];
+        
+        error = (zvFinal(1,1)-y(h))**2;
+        results{lag}.memory{m}.h{h}.error = [results{lag}.memory{m}.h{h}.error; error];
+        if (error < mse)
+          mse = error;
+          zvBest = zvFinal;
+          hBest = h;
+          mnseBest = mnse;
+          maeBest = mae;
+          rBest = r;
+        end
       end
-    end    
+    end
+        
   end
-  
 end
 
 hold on;
-plot(1:length(zvBest),y(mejorMem-1+hBest:end));
+plot(1:length(zvBest),y(memories-1+hBest:end));
 plot(1:length(zvBest),zvBest,"r");
 %plot(1:columns(results{lag}.zv),results{lag}.zv,"r");
 legend('Actual Value','Estimated Value');
 hold off;
 
-datatst = y(mejorMem-1+hBest:end);
+datatst = y(length(memories)-1+hBest:end);
 pronostico = zvBest';
 p = polyfit(pronostico,datatst((length(datatst) - length(pronostico))+1:end),1) %% saco los valores y = 32173612873912x + b (los de antes de x y el b)
 f = polyval(p,pronostico);
@@ -139,7 +136,7 @@ plot(datatst((length(datatst) - length(pronostico))+1:end),datatst((length(datat
 hold off; 
 xlabel('X');
 ylabel('Y');
-%title("AR");
+title("AR");
 legend("data point", "best linear fit", "Y=X")
 set(gca,'XTick',[0:5:100]);
 set(gca,'YTick',[0:5:100]);
